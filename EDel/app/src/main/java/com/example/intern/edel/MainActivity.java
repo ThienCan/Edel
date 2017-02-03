0a6bcda1cd8909ddb1de381bed916a00f8abcbf9
thien.tranminh <minhthientran2501@gmail.com>
2017-02-03 17:17:59 +0700

    Sign in and Sign up layout

---
+++ EDel/app/src/main/java/com/example/intern/edel/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.intern.edel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void connect_with_facebook_account(View view){
+
+    }
+    public void logIn(View view){
+
+    }
+    public void signUp(View view){
+
+    }
+
+}
---
+++ EDel/app/src/main/java/com/example/intern/edel/Signup.java
@@ -0,0 +1,17 @@
+package com.example.intern.edel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+    public void creat_new_account(View view){
+
+    }
+}
